0e6dd2fa570576080235fc8909d5a53cb0650b5f
Mr. Magma <joshuamagma2013@gmail.com>
2017-05-01 11:59:13 -0500

    Add extra data for specific purposes

--- folder-creator.py
+++ folder-creator.py
@@ -8,10 +8,29 @@ def mkdir(dirname):
 with open("./CCSS/data.json") as f:
     data = json.load(f)
 
+def write_children(base_dir, data):
+    children = []
+    
+    for child in data["children"]:
+        child2 = {}
+        for key in child:
+            if key == "children":
+                child2[key] = len(child[key])
+            else:
+                child2[key] = child[key]
+        children.append(child2)
+    
+    f = open(base_dir + "/children.json", "w")
+    f.write(json.dumps(children))
+    f.close()
+
 def create_subdirs(base_dir, data):
     f = open(base_dir + "/data.json", "w")
     f.write(json.dumps(data))
     f.close()
+    
+    write_children(base_dir, data)
+    
     for child in data["children"]:
         mkdir(base_dir + "/" + child["standard_slug"])
         create_subdirs(base_dir + "/" + child["standard_slug"], child)
